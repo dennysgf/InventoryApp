aa5821e57db01dd1c42dd8309f6d5e6a1c087a92
dennysbelduma <dennysbelduma98@gmail.com>
2025-09-18 21:30:44 -0500

    configuration

--- TransactionService/obj/Debug/net8.0/TransactionService.AssemblyInfo.cs
+++ TransactionService/obj/Debug/net8.0/TransactionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransactionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01d39524880c0cccca3e12c2dc88397b0e59ac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e926bb8c9e591bdbda7bfa5c1877565c763d45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransactionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransactionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
